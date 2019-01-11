8aeb786d1f3d62f7b986a26e59206761deb3ee57
logavignesh98 <46574124+logavignesh98@users.noreply.github.com>
2019-01-11 09:17:40 +0530

    Create check whether a character is vowels or consonant.py

---
+++ check whether a character is vowels or consonant.py
@@ -0,0 +1,7 @@
+s= raw_input()
+if (s=='a' or s=='e' or s=='i' or s=='o' or s=='u' or s=='A' or s=='E' or s=='I' or s=='O' or s=='U'):
+	print("vowels")
+elif (s=='@' or s=='#' or s=='$' or s=='%'):
+	print("consonant")
+else:
+	print("not vowels")
