c60f71976de47462cc0137ff78c26ac533fd0477
logavignesh98 <46574124+logavignesh98@users.noreply.github.com>
2019-01-24 23:34:14 +0530

    Update Given 2 numbers N,K and an array of an integer,find sum of the first k integers.py

--- Given 2 numbers N,K and an array of an integer,find sum of the first k integers.py
+++ Given 2 numbers N,K and an array of an integer,find sum of the first k integers.py
@@ -1,7 +1,5 @@
-def ksum(arr,k)
-    arr.sort(reverse=true)
-    for i in range(k):
-        print(arr[i],end=" ")
-arr = [2,6,23,29,35,48,54]
-k = 3
-ksum(arr,k)
+N =int(input())
+K =int(input())
+a =[1,2,3,4,5]
+c =a[0]+a[1]
+print (c)
